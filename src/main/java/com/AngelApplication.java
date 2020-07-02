33b0d76f668702b4d656140593971daa0bec9a5b
gongdaoyi <925622722@qq.com>
2020-07-02 11:22:18 +0800

    2020年7月2日

--- src/main/java/com/AngelApplication.java
+++ src/main/java/com/AngelApplication.java
@@ -1,9 +1,7 @@
 package com;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AngelApplication {
